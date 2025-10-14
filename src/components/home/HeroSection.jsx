b362ef4536940c27e852c2c75442785db9fb49dd
Edanaichel <team55eyo@gmail.com>
2025-10-13 17:03:37 -0700

    Revert "Adjust mobile hero background positioning"
    This reverts commit 931c35fd4ff726873c9d1380329ee8dff751aaeb.

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -19,11 +19,10 @@ export default function HeroSection() {
       
       {/* Background Image - Mobile */}
       <div 
-        className="absolute inset-0 block md:hidden"
+        className="absolute inset-0 bg-center block md:hidden"
         style={{
           backgroundImage: "url('https://i.imgur.com/7yHNm12.jpeg')",
-          backgroundSize: "120%",
-          backgroundPosition: "30% center",
+          backgroundSize: "70%",
           backgroundRepeat: "no-repeat",
         }}
       >
